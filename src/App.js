c7643b48d01cc8342cd543c4597b355fe1244518
OlegSuprunovich <olsupr@ukr.net>
2022-09-18 21:38:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         < Weather />
-        <a href="https://github.com/Tasupr/react-weather-app" target={_blank}>My GitHub link here</a>
+        <a href="https://github.com/Tasupr/react-weather-app">My GitHub link here</a>
       </header>
     </div>
   );
